55144cc2e8c9ab9d4dd4dbf68564d6f73003ba46
Geoffrey Emerit <geoffrey.emerit@gmail.com>
2022-05-17 19:17:56 +0200

    [UPDATE] Footer completed + responsive ok

---
+++ data/footLinks.js
@@ -0,0 +1,32 @@
+const footLinks = [
+  {
+    id: "logos-facebook",
+    name: "Facebook",
+    href: "https://www.facebook.com/geoffrey.emerit/",
+    target: "_blank",
+  },
+  {
+    id: "logos-github",
+    name: "Github",
+    href: "https://github.com/geoffreyemerit",
+    target: "_blank",
+  },
+  {
+    id: "logos-linkedin",
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/geoffreyemerit/",
+    target: "_blank",
+  },
+  {
+    id: "logos-mail",
+    name: "Email",
+    href: "mailto:geoffrey.emerit@gmail.com",
+  },
+  {
+    id: "logos-phone",
+    name: "Phone",
+    href: "tel:0622175004",
+  },
+];
+
+export default footLinks;
